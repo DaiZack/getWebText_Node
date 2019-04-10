21a4dca2265c0012d61fa6effe07ace8f42590d3
Zack Dai <45176371+zdrel8ed@users.noreply.github.com>
2019-04-10 00:39:24 -0400

    Update IDC.js

--- IDC.js
+++ IDC.js
@@ -2,6 +2,7 @@ const csv = require('fast-csv')
 const request = require('request')
 const cheerio = require('cheerio')
 const fs = require('fs')
+require('events').EventEmitter.defaultMaxListeners = Infinity;
 
 function get_links(url) {
     return new Promise(resolve => {
@@ -35,14 +36,16 @@ function get_text(url) {
     return new Promise(resolve => {
         request.get({ url: url, followAllRedirects: true }, (err, res, body) => {
             if (err) { console.log('err', url) } else {
-                var $ = cheerio.load(body)
-                drop_tag = ['style', 'script', 'footer', 'header','iframe', 'head', 'title', 'meta', '[document]']
-                for (tag in drop_tag) {
-                    $(tag).remove()
-                }
-                var text = $('body').text();
-                text = text.replace(/[\n\r\s\|]+/g, ' ')
-                return resolve(text)
+                if(res.statusCode===200){
+                    var $ = cheerio.load(body)
+                    drop_tag = ['style', 'script', 'footer', 'header', 'head', 'title', 'meta', '[document]']
+                    for (tag in drop_tag) {
+                        $(tag).remove()
+                    }
+                    var text = $('body').text();
+                    text = text.replace(/[\n\r\s\|]+/g, ' ')
+                    return resolve(text)
+                }    
             }
         })
     })
@@ -50,19 +53,16 @@ function get_text(url) {
 
 
 async function get_pages(url) {
-    var links = await get_links(url);
-    links.push(url);
-    var texts = '';
-    links.forEach(async (link) => {
-        var text = await get_text(link)
-        console.log('get', link)
-        texts += text;
-        texts.replace('\n', ' ')
-
-        fs.writeFileSync('result/'+url.replace('http://www.', '')+'..json',JSON.stringify({ webset: url.replace('http://www.', ''), WebText: texts }))
-        
-    })
-}
+        var links = await get_links(url);
+        links.push(url);
+        var promises = links.map(get_text)
+        var texts = await Promise.all(promises)
+        texts = [...new Set(texts)]
+        ntexts = texts.join(' ').replace(/[\n\s\r\|]+/g, ' ')
+        if(ntexts.length>200){
+            fs.writeFileSync('result/'+url.replace('http://www.', '')+'..json',JSON.stringify({ webset: url.replace('http://www.', ''), WebText: ntexts }))
+        }
+    }
 
 csv.fromPath("O:\\rel8edto\\DataC\\IDC\\list.csv", { headers: true })
     .on("data", data => {
@@ -70,3 +70,18 @@ csv.fromPath("O:\\rel8edto\\DataC\\IDC\\list.csv", { headers: true })
         console.log(url)
         get_pages(url)
     })
+
+
+// function allpages(){
+//     let urls=[]
+//     csv.fromPath("O:\\rel8edto\\DataC\\IDC\\list.csv", { headers: true })
+//         .on("data", data => {
+//             url = 'http://www.' + data.Website
+//             urls.push(url)
+//         })
+//         .on('end',()=>{ 
+//             urls.map(get_pages);
+//         })
+// }
+
+// allpages()
